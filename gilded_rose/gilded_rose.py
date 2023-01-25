fd162a99f4d7a13ac3e43aaa53c927b055c3ba17
bytesontherocks <bytes@dimoni>
2023-01-25 11:48:25 +0000

    chore: update sell in

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -3,8 +3,14 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def _update_sell_in(self, item):
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            item.sell_in = item.sell_in - 1
+        
     def update_quality(self):
         for item in self.items:
+            self._update_sell_in(item)
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
@@ -13,14 +19,13 @@ class GildedRose(object):
                 if item.quality < 50:
                     item.quality = item.quality + 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
+                        if item.sell_in < 10:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-                        if item.sell_in < 6:
+                        if item.sell_in < 5:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+        
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
@@ -28,7 +33,7 @@ class GildedRose(object):
                             if item.name != "Sulfuras, Hand of Ragnaros":
                                 item.quality = item.quality - 1
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
